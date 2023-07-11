bdf62ff14fba87aa96ee2e51269fab86a0601b9c
Pawel Szafran <pawel.szafran@gmail.com>
2023-07-11 09:05:06 +0200

    Planck: add temp reset on base

--- keyboards/planck/keymaps/pawel-szafran/keymap.c
+++ keyboards/planck/keymaps/pawel-szafran/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,  _______,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,  _______,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,  _______,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-    _______, _______, KC_LGUI, LOWER,   KC_SPC,  _______,  _______,  KC_SPC,  RAISE,   _______, _______, _______ 
+    QK_BOOT, _______, KC_LGUI, LOWER,   KC_SPC,  _______,  _______,  KC_SPC,  RAISE,   _______, _______, _______ 
 ),
 
 /* Lower
