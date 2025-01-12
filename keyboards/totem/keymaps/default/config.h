ffde5e8e61a23616239a0a7c7cf211e76c8f1b99
Pawel Szafran <pawel.szafran@gmail.com>
2025-01-12 22:52:00 +0100

    Update Totem firmware

--- keyboards/totem/keymaps/default/config.h
+++ keyboards/totem/keymaps/default/config.h
@@ -11,9 +11,6 @@ for more options.
 //#define TAPPING_TERM 170
 #define TAPPING_TERM 170
 
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
-
 // Auto Shift
 #define NO_AUTO_SHIFT_ALPHA
 #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
@@ -22,4 +19,3 @@ for more options.
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
 #define NO_ACTION_ONESHOT
-
